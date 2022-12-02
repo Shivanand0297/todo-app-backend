7e586990f2657eea02b0a0d82f52cb93aa56525d
Shivanand <Shivanand0297@gmail.com>
2022-12-03 01:16:29 +0530

    added create task route and controller

---
+++ controllers/createTask.js
@@ -0,0 +1,32 @@
+const Todo = require("../models/todos")
+
+exports.createTask = async (req, res)=>{
+    try {
+        // first grabing the todo in which task is to be created
+        const todo = await Todo.findById(req.params.id)
+
+        if(!todo){
+            res.status(400).send("Cannot find your todo")
+        }
+
+        // taking task from the frontend
+        const {task} = req.body;
+        
+        // appending the task in the task array of the above todo
+        todo.task.push(task)
+
+        // saving the todo
+        await todo.save();
+
+        res.status(201).json({
+            success: true, 
+            message: "task created successfully",
+            todo
+        })
+    } catch (error) {
+        res.status(401).json({
+            message: error.message, 
+            status: "Error in createTodo controller"
+        })
+    }
+}
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTask } = require("../controllers/createTask");
 const { createTodo } = require("../controllers/createTodo");
 const { getTodos } = require("../controllers/getTodos");
 const { home } = require("../controllers/home");
@@ -7,5 +8,6 @@ const router = express.Router();
 router.get("/", home)
 router.post("/createTodo", createTodo)
 router.get("/getTodos", getTodos)
+router.post("/createTask/:id", createTask)
 
 module.exports = router
