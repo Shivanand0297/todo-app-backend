2c1a0365b13add9265c7e369b2f06a789d7fa91f
Shivanand <Shivanand0297@gmail.com>
2022-12-03 00:38:08 +0530

    added home route and fixed server

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { createTodo } = require("../controllers/createTodo");
 const { getTodos } = require("../controllers/getTodos");
+const { home } = require("../controllers/home");
 const router = express.Router();
 
+router.get("/", home)
 router.post("/createTodo", createTodo)
 router.get("/getTodos", getTodos)
 
