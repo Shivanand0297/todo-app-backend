b79a3b98887da345963e6b541aaf5fabc97d9496
Shivanand <Shivanand0297@gmail.com>
2022-12-03 00:36:49 +0530

    added home route

---
+++ controllers/home.js
@@ -0,0 +1,12 @@
+const Todo = require("../models/todos")
+
+exports.home = async (req, res)=>{
+    try {
+        res.status(201).send("welcome to home page")
+    } catch (error) {
+        res.status(401).json({
+            message: error.message, 
+            status: "Error in home controller"
+        })
+    }
+}
