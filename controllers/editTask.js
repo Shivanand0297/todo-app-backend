abaacf68ed4abd3a6a5373ea74ecf279e7d0a473
Shivanand <Shivanand0297@gmail.com>
2022-12-03 12:56:31 +0530

    added edit Task controller

---
+++ controllers/editTask.js
@@ -0,0 +1,38 @@
+const Todo = require("../models/todos")
+
+exports.editTask = async (req, res)=>{
+    try {
+        // grabbing the todo
+        const todo = await Todo.findById(req.params.id);
+
+        // validating if todo is there or not
+        if(!todo){
+            res.status(401).send("Could not found any todo with this id")
+        }
+
+        // taking task index and updated task
+        const {taskIndex, updatedTask} = req.body;
+        const taskArrayIndex = Number(taskIndex)
+        // checking if task index is number or not
+        if(typeof(taskArrayIndex) !== "number"){
+            console.log("task index is not number");
+            res.send("task index is not number")
+        }
+
+        // updating the task in the array
+        todo.task[taskArrayIndex] = updatedTask;
+
+        // saving the todo
+        await todo.save();
+        res.status(201).json({
+            success: true, 
+            message: "task updated successfully",
+            todo
+        })
+    } catch (error) {
+        res.status(401).json({
+            message: error.message, 
+            status: "Error in edit task controller"
+        })
+    }
+}
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createTask } = require("../controllers/createTask");
 const { createTodo } = require("../controllers/createTodo");
+const { editTask } = require("../controllers/editTask");
 const { editTodo } = require("../controllers/editTodo");
 const { getTodos } = require("../controllers/getTodos");
 const { home } = require("../controllers/home");
@@ -11,5 +12,5 @@ router.post("/createTodo", createTodo)
 router.get("/getTodos", getTodos)
 router.post("/createTask/:id", createTask)
 router.put("/editTodo/:id", editTodo)
-
+router.patch("/editTask/:id", editTask)
 module.exports = router
