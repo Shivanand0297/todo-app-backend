976c61e83265e9c593ea2a5b40daddaa53584b9e
Shivanand <Shivanand0297@gmail.com>
2022-12-04 16:11:24 +0530

    added some comments

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
 // bringing all the env variables
 require("dotenv").config()
 
+// bringing express
+const express = require("express")
+
+// initializing cors for enabling cross server access
+const cors = require("cors")   
+
 // connecting with the database before starting the database
 require("./config/dbConnect").dbConnect()
 
-// initializing cors for enabling cross server access
-const cors = require("cors")     
-
 // bringing routes
+const todoRoutes = require("./routes/todoRoutes")
+
 const corsOptions = {
     origin: "http://localhost:3000"
 }
-const express = require("express")
+
 const app = express()
 
-const todoRoutes = require("./routes/todoRoutes")
-
 // middleware
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(cors(corsOptions))
 
 // routes
 app.use("/", todoRoutes)
 
 module.exports = app
+
+
+/**
+ * require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+require("./config/dbConnect").connectToDb()
+const userRoutes = require("./routes/userRoutes")
+
+const corsOptions ={
+    origin:'http://localhost:3000'
+}
+
+const app = express()
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use("/", userRoutes)
+
+module.exports = app
+ */
