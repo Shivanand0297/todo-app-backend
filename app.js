614e20f9bffa62384bb3e3411e412a62d3848ff2
Shivanand <Shivanand0297@gmail.com>
2022-12-04 16:12:41 +0530

    added some comments

--- app.js
+++ app.js
@@ -28,24 +28,3 @@ app.use(express.urlencoded({extended: true}))
 app.use("/", todoRoutes)
 
 module.exports = app
-
-
-/**
- * require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-require("./config/dbConnect").connectToDb()
-const userRoutes = require("./routes/userRoutes")
-
-const corsOptions ={
-    origin:'http://localhost:3000'
-}
-
-const app = express()
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use("/", userRoutes)
-
-module.exports = app
- */
