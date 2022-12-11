aa5083dd1d85fdd483c3377c564cb0ddc688d7b5
Shivanand <Shivanand0297@gmail.com>
2022-12-11 13:43:04 +0530

    Added user Schema

---
+++ models/Users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const {Schema} = mongoose;
+const UserSchema = new Schema(
+    {
+        firstname: {
+            type: String, 
+            required: true,
+            maxLength: [20, "Firstname should not be more then 20 characters"],
+        },
+        lastname: {
+            type: String,
+            required: true, 
+            maxLength: [20, "Lastname should not be more then 20 characters"],
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+        
+    }
+)
+module.exports = mongoose.model("users", UserSchema)
